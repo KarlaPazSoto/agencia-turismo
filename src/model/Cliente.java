c4c055b377bd24d5ae87252940dc7b08bb2dc57e
Karla Paz Soto <ksoto.albornoz@gmail.com>
2026-06-04 17:44:34 -0400

    creacion codigo clase cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,4 +1,63 @@
 package model;
 
-public class Cliente{
+/**
+ * Representa a un cliente de la agencia
+ */
+public class Cliente extends Persona{
+
+    //atributo privado (propio del cliente)
+    private String tourReservado;
+
+    /**
+     * Constructor vacio
+     */
+    public Cliente() {
+
+    }
+
+    /**
+     * Constructor con parametros
+     *
+     * @param nombre nombre del cliente
+     * @param rut rut del cliente
+     * @param direccion direccion del cliente
+     * @param tourReservado tour reservado por el cliente
+     */
+    public Cliente(String nombre, String rut, Direccion direccion, String tourReservado) {
+        super(nombre, rut, direccion);
+        this.tourReservado = tourReservado;
+    }
+
+    /**
+     * Obtiene el tour reservado por el cliente
+     *
+     * @return tour reservado
+     */
+    public String getTourReservado() {
+        return tourReservado;
+    }
+
+    /**
+     * Modifica el tour reservado por el cliente
+     *
+     * @param tourReservado nuevo tour reservado
+     */
+    public void setTourReservado(String tourReservado) {
+        this.tourReservado = tourReservado;
+    }
+
+    /**
+     * Devuelve la informacion del cliente y el tour reservado en formato de texto legible
+     *
+     * @return informcion del cliente y el tour
+     */
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", rut='" + rut + '\'' +
+                ", direccion=" + direccion +
+                ", Tour reservado=" + tourReservado +
+                '}';
+    }
 }
