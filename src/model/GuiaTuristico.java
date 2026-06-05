ec15407d16a6a734aafb653d0e6da23c04095ce1
Karla Paz Soto <ksoto.albornoz@gmail.com>
2026-06-05 16:11:05 -0400

    creacion codigo clase guia turitico

--- src/model/GuiaTuristico.java
+++ src/model/GuiaTuristico.java
@@ -1,4 +1,30 @@
 package model;
 
-public class GuiaTuristico {
+public class GuiaTuristico extends Persona{
+
+    private String idioma;
+
+    public GuiaTuristico(){
+
+    }
+
+    public GuiaTuristico(String nombre, String rut, Direccion direccion, String idioma){
+        super(nombre, rut, direccion);
+        this.idioma = idioma;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    @Override
+    public String toString() {
+        return "GuiaTuristico{" +
+                "idioma='" + idioma + '\'' +
+                '}';
+    }
 }
