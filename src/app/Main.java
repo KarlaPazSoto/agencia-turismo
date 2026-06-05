874f9747ad2ae6fe576ae0546b0128572728590e
Karla Paz Soto <ksoto.albornoz@gmail.com>
2026-06-05 16:30:43 -0400

    creacion codigo clase main y documentacion clase guia turistico

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,17 +1,33 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Cliente;
+import model.Direccion;
+import model.GuiaTuristico;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Direcciones
+        Direccion direccion1 = new Direccion("calle uno", 87, "Puerto Varas");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Direccion direccion2 = new Direccion("calle dos", 380, "Frutillar");
+
+        Direccion direccion3 = new Direccion("calle tres", 94, "Puerto Montt");
+
+        //Clientes
+        Cliente cliente1 = new Cliente("Karla Soto", "20387474-9", direccion1, "Volcán Osorno");
+
+        Cliente cliente2 = new Cliente("Matias Flores","18387646-2", direccion2, "Saltos del Petrohué");
+
+        //Guia
+        GuiaTuristico guia1 = new GuiaTuristico("Katherine Espinosa", "20765382-6", direccion3, "Portugués");
+
+        //Salidas por consola
+        System.out.println(cliente1);
+        System.out.println();
+
+        System.out.println(cliente2);
+        System.out.println();
+
+        System.out.println(guia1);
     }
 }
--- src/model/GuiaTuristico.java
+++ src/model/GuiaTuristico.java
@@ -1,26 +1,55 @@
 package model;
 
+/**
+ * Representa a un guia turistico de la agencia
+ */
 public class GuiaTuristico extends Persona{
-
+    //atributo privado propio del guia turistico
     private String idioma;
 
+    /**
+     * Constructor vacio
+     */
     public GuiaTuristico(){
 
     }
 
+    /**
+     * Constructor con parametros
+     *
+     * @param nombre nombre del guia turistico
+     * @param rut rut del guia turistico
+     * @param direccion direccion del guia turistico
+     * @param idioma del guia turistico
+     */
     public GuiaTuristico(String nombre, String rut, Direccion direccion, String idioma){
         super(nombre, rut, direccion);
         this.idioma = idioma;
     }
 
+    /**
+     * Obtiene el idioma que maneja el guia turistico
+     *
+     * @return idioma
+     */
     public String getIdioma() {
         return idioma;
     }
 
+    /**
+     * Modifica el idioma que maneja el guia turistico
+     *
+     * @param idioma nuevo idioma
+     */
     public void setIdioma(String idioma) {
         this.idioma = idioma;
     }
 
+    /**
+     * Devuelve la informacion del guia turistico en formato de texto legible
+     *
+     * @return informacion de guia turistico
+     */
     @Override
     public String toString() {
         return "GuiaTuristico{" +
