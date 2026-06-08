d261158378f3064f8ec8d500a5b03d5f9e1f4dd1
Karla Paz Soto <ksoto.albornoz@gmail.com>
2026-06-08 15:54:31 -0400

    integrando excepciones a la clase main

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,31 +3,39 @@ package app;
 import model.Cliente;
 import model.Direccion;
 import model.GuiaTuristico;
+import utils.ValidacionException;
 
+/**
+ * Clase principal del sistema
+ */
 public class Main {
     public static void main(String[] args) {
-        //Direcciones
-        Direccion direccion1 = new Direccion("calle uno", 87, "Puerto Varas");
+        try {
+            //Direcciones
+            Direccion direccion1 = new Direccion("calle uno", 87, "Puerto Varas");
 
-        Direccion direccion2 = new Direccion("calle dos", 380, "Frutillar");
+            Direccion direccion2 = new Direccion("calle dos", 380, "Frutillar");
 
-        Direccion direccion3 = new Direccion("calle tres", 94, "Puerto Montt");
+            Direccion direccion3 = new Direccion("calle tres", 94, "Puerto Montt");
 
-        //Clientes
-        Cliente cliente1 = new Cliente("Karla Soto", "20387474-9", direccion1, "Volcán Osorno");
+            //Clientes
+            Cliente cliente1 = new Cliente("Karla Soto", "20387474-9", direccion1, "Volcán Osorno");
 
-        Cliente cliente2 = new Cliente("Matias Flores","18387646-2", direccion2, "Saltos del Petrohué");
+            Cliente cliente2 = new Cliente("Matias Flores","18387646-2", direccion2, "Saltos del Petrohué");
 
-        //Guia
-        GuiaTuristico guia1 = new GuiaTuristico("Katherine Espinosa", "20765382-6", direccion3, "Portugués");
+            //Guia
+            GuiaTuristico guia1 = new GuiaTuristico("Katherine Espinosa", "20765382-6", direccion3, "Portugués");
 
-        //Salidas por consola
-        System.out.println(cliente1);
-        System.out.println();
+            //Mostrar informacion
+            System.out.println(cliente1);
+            System.out.println();
 
-        System.out.println(cliente2);
-        System.out.println();
+            System.out.println(cliente2);
+            System.out.println();
 
-        System.out.println(guia1);
+            System.out.println(guia1);
+        } catch (ValidacionException e) {
+            System.out.println("Error de validación: " + e.getMessage());
+        }
     }
 }
